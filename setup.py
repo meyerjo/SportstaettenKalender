ca6531b466ee1dc7a853596aca9229d1cd1736d5
bojome <meyerjo@informatik.uni-freiburg.de>
2016-01-06 16:24:54 +0100

    * added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='StadtKalenderParsen',
+        version='0.1',
+        packages=[''],
+        url='',
+        license='',
+        author='Johannes Meyer',
+        author_email='',
+        description='',
+        requires=['icalendar', 'jsonpickle', 'beautifulsoup4']
+)
